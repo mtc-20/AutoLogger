b45f2a5db658e71f4ef56b834af42cb29d1a53bd
Abir Bhattacharyya <46240907+bathonSpidey@users.noreply.github.com>
2019-11-28 16:42:55 +0100

    V1.1

--- auto_logger.py
+++ auto_logger.py
@@ -11,12 +11,13 @@ import numpy as np
 import os
 import glob
 from datetime import datetime
-
-
+import math
+import time
 
 faces=glob.glob("faces/*.jpg")
-print(faces[0].find('.'))
-print(faces[0][5+1:10])
+print(faces)
+#print(faces[0].find('.'))
+#print(faces[0][5+1:10])
 # This is a demo of running face recognition on live video from your webcam. It's a little more complicated than the
 # other example, but it includes some basic performance tweaks to make things run a lot faster:
 #   1. Process each video frame at 1/4 resolution (though still display it at full resolution)
@@ -41,7 +42,7 @@ for face in faces:
 
     
 # Load a sample picture and learn how to recognize it.
-    '''
+'''
 obama_image = face_recognition.load_image_file("obama.jpg")
 obama_face_encoding = face_recognition.face_encodings(obama_image)[0]
 
@@ -66,7 +67,7 @@ face_names = []
 '''
 process_this_frame = True
 count=0
-workers=['Abir']
+workers=['Abir','Quang', 'Thomas', 'Prof Hartanto']
 while True:
     # Grab a single frame of video
     ret, frame = video_capture.read()
@@ -120,9 +121,10 @@ while True:
         cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
         font = cv2.FONT_HERSHEY_DUPLEX
         cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+    print(name)
     if name in workers:
         count+=1
-        if count==10:
+        if count==20:
             break
     # Display the resulting image
     cv2.imshow('Video', frame)
@@ -135,7 +137,175 @@ while True:
 video_capture.release()
 cv2.destroyAllWindows()
 
-name='quang'
+
+#Putting coming in and going out logic
+time.sleep(2)
+cap = cv2.VideoCapture(0)
+entry=''  
+count_in=0
+count_out=0 
+while(True):
+    try:    
+        ret, frame = cap.read()
+        frame=cv2.flip(frame,1)
+        kernel = np.ones((3,3),np.uint8)
+        
+        #define region of interest
+        roi=frame[100:300, 100:300]
+        
+        
+        cv2.rectangle(frame,(100,100),(300,300),(0,255,0),0)    
+        hsv = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
+        
+        
+         
+    # define range of skin color in HSV
+        lower_skin = np.array([0,20,70], dtype=np.uint8)
+        upper_skin = np.array([20,255,255], dtype=np.uint8)
+        
+     #extract skin colur imagw  
+        mask = cv2.inRange(hsv, lower_skin, upper_skin)
+        
+       
+        
+    #extrapolate the hand to fill dark spots within
+        mask = cv2.dilate(mask,kernel,iterations = 4)
+        
+    #blur the image
+        mask = cv2.GaussianBlur(mask,(5,5),100) 
+        
+        
+        
+    #find contours
+        contours,hierarchy= cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    
+       #find contour of max area(hand)
+        cnt = max(contours, key = lambda x: cv2.contourArea(x))
+        
+    #approx the contour a little
+        epsilon = 0.0005*cv2.arcLength(cnt,True)
+        approx= cv2.approxPolyDP(cnt,epsilon,True)
+       
+        
+    #make convex hull around hand
+        hull = cv2.convexHull(cnt)
+        
+     #define area of hull and area of hand
+        areahull = cv2.contourArea(hull)
+        areacnt = cv2.contourArea(cnt)
+      
+    #find the percentage of area not covered by hand in convex hull
+        arearatio=((areahull-areacnt)/areacnt)*100
+    
+     #find the defects in convex hull with respect to hand
+        hull = cv2.convexHull(approx, returnPoints=False)
+        defects = cv2.convexityDefects(approx, hull)
+        
+    # l = no. of defects
+        l=0
+        
+    #code for finding no. of defects due to fingers
+        for i in range(defects.shape[0]):
+            s,e,f,d = defects[i,0]
+            start = tuple(approx[s][0])
+            end = tuple(approx[e][0])
+            far = tuple(approx[f][0])
+            pt= (100,180)
+            
+            
+            # find length of all sides of triangle
+            a = math.sqrt((end[0] - start[0])**2 + (end[1] - start[1])**2)
+            b = math.sqrt((far[0] - start[0])**2 + (far[1] - start[1])**2)
+            c = math.sqrt((end[0] - far[0])**2 + (end[1] - far[1])**2)
+            s = (a+b+c)/2
+            ar = math.sqrt(s*(s-a)*(s-b)*(s-c))
+            
+            #distance between point and convex hull
+            d=(2*ar)/a
+            
+            # apply cosine rule here
+            angle = math.acos((b**2 + c**2 - a**2)/(2*b*c)) * 57
+            
+        
+            # ignore angles > 90 and ignore points very close to convex hull(they generally come due to noise)
+            if angle <= 90 and d>30:
+                l += 1
+                cv2.circle(roi, far, 3, [255,0,0], -1)
+            
+            #draw lines around hand
+            cv2.line(roi,start, end, [0,255,0], 2)
+            
+            
+        l+=1
+        
+        #print corresponding gestures which are in their ranges
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        if l==1:
+            if areacnt<2000:
+                cv2.putText(frame,'Put rock for coming in and any other for going out inside the box',(0,50), font, 2, (0,0,255), 3, cv2.LINE_AA)
+            else:
+                if arearatio<12:
+                    cv2.putText(frame,'Could not recognize try again',(0,50), font, 2, (0,0,255), 3, cv2.LINE_AA)
+                elif arearatio<17.5:
+                    cv2.putText(frame,'Best of luck',(0,50), font, 2, (0,0,255), 3, cv2.LINE_AA)
+            
+                    count_in+=1
+                   
+                else:
+                    cv2.putText(frame,'Could not recognize try again',(0,50), font, 2, (0,0,255), 3, cv2.LINE_AA)
+                    
+        elif l==2:
+            cv2.putText(frame,'Could not recognize try again',(0,50), font, 2, (0,0,255), 3, cv2.LINE_AA)
+            
+        elif l==3:
+         
+              if arearatio<27:
+                    cv2.putText(frame,'Could not recognize try again',(0,50), font, 2, (0,0,255), 3, cv2.LINE_AA)
+              else:
+                    cv2.putText(frame,'Could not recognize try again',(0,50), font, 2, (0,0,255), 3, cv2.LINE_AA)
+                    
+        elif l==4:
+            cv2.putText(frame,'Going out registered',(0,50), font, 2, (0,0,255), 3, cv2.LINE_AA)
+            count_out+=1
+            
+        elif l==5:
+            cv2.putText(frame,'Going out registered',(0,50), font, 2, (0,0,255), 3, cv2.LINE_AA)
+            count_out+=1
+            
+        elif l==6:
+            cv2.putText(frame,'reposition',(0,50), font, 2, (0,0,255), 3, cv2.LINE_AA)
+            
+        else :
+            cv2.putText(frame,'reposition',(10,50), font, 2, (0,0,255), 3, cv2.LINE_AA)
+            
+        if count_in==20:
+            entry+='in'
+            break
+        elif count_out==20:
+            entry+='out'
+            break
+            
+        #show the windows
+        cv2.imshow('mask',mask)
+        cv2.imshow('frame',frame)
+    except:pass
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):break
+    
+cv2.destroyAllWindows()
+cap.release()   
+
+
+
+
+
+
+
+
+
+
+
+
 now=datetime.now()
 log=os.listdir('logbook/')
 month=now.strftime("%B")
@@ -144,18 +314,38 @@ day=str(now.day)
 hour=str(now.hour)
 minute=str(now.minute)
 print(log)
+
+
 if month+' '+year not in log:
     os.mkdir('logbook/'+(now.strftime("%B")+' '+year))
 day_glob=os.listdir("logbook/"+month+' '+year)
 print(day_glob)
 file_path="logbook/"+(now.strftime("%B")+' '+year)
-file_name=day+'.txt'
+file_name=day+'-'+month+'-'+year+'.txt'
 full_path=os.path.join(file_path, file_name)
-if day not in day_glob:
+if day+'-'+month+'-'+year+'.txt' not in day_glob:
     f=open(full_path,"a+")
-    f.write(name+': '+hour+':'+minute+'\n')
+    if 'in' in entry:
+        f.write('[Check In++] Username: '+name+' '+day+'-'+ str(now.month) +'-'+year+' '+ str(now.strftime("%H:%M"))+'\n')
+    else:
+        f.write('[--Check Out] Username: '+name+' '+day+'-'+ str(now.month) +'-'+year+' '+str(now.strftime("%H:%M"))+'\n') 
 else:
-    f=open(full_path,"a+")
-    f.write(name+': '+hour+':'+minute+'\n')
+    f=open(full_path,"r")
+    a=f.read()
+    #f=open(full_path,"a+")
+    if 'in' in entry:
+        if '[Check In] Username: '+name in a:
+            print('You are already in. Try again with hi-five to go out')
+        else:
+            g=open(full_path,"a+")
+            g.write('[Check In++] Username: '+name+' '+day+'-'+ str(now.month) +'-'+year+' '+ str(now.strftime("%H:%M"))+'\n')
+            g.close()
+    else:
+        if '[Check Out] Username: '+name in a:
+            print('You are already out. Try again with fist bump to come in')
+        else:
+             g=open(full_path,"a+")
+             g.write('[--Check Out] Username: '+name+' '+day+'-'+ str(now.month) +'-'+year+' '+str(now.strftime("%H:%M"))+'\n')
+             g.close()
     
 f.close()
